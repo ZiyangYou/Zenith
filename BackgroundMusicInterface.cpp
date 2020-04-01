dab85fa5143d72a7a0fe414b6cd9ffda8e1d68be
Ziyang(Arvin) You <48134119+ZiyangYou@users.noreply.github.com>
2020-04-01 15:35:34 -0700

    Delete BackgroundMusicInterface.cpp

--- BackgroundMusicInterface.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BackgroundMusicInterface.h"
-
-// Add default functionality here for any IBackgroundMusicInterface functions that are not pure virtual.
